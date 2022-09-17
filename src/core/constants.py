9f2098047f85f27b6ca50dfac0858f2f1ed5157e
Batuhan Şanlı <batuhansanli@gmail.com>
2022-09-17 16:30:28 +0300

    add core/constans

---
+++ src/core/constants.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+ROOT_FOLDER_NAME = ".browext"
+
+PROJECT_ROOT_PATH = os.path.join(sys.path[0], ROOT_FOLDER_NAME)
+HOME_ROOT_PATH = os.path.join(os.path.expanduser("~"), ROOT_FOLDER_NAME)

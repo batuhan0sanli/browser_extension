30fb3ab5c0e0ff7983e09c47e248e166538762f0
Batuhan Şanlı <batuhansanli@gmail.com>
2022-09-18 21:42:31 +0300

    add some variables for download modules

--- src/core/config.py
+++ src/core/config.py
@@ -12,3 +12,4 @@ load_dotenv()
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 DOWNLOAD_PATH = os.getenv("DOWNLOAD_PATH", HOME_ROOT_PATH)
+CHUNK_SIZE = os.getenv("CHUNK_SIZE", 32 * 1024)  # byte
--- src/core/constants.py
+++ src/core/constants.py
@@ -5,3 +5,9 @@ ROOT_FOLDER_NAME = ".browext"
 
 PROJECT_ROOT_PATH = os.path.join(sys.path[0], ROOT_FOLDER_NAME)
 HOME_ROOT_PATH = os.path.join(os.path.expanduser("~"), ROOT_FOLDER_NAME)
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
+
+HEADERS = {
+    "User-Agent": USER_AGENT
+}

cbd123a1cd76cc106a3fd90fef86cd8bc150072d
Batuhan Şanlı <batuhansanli@gmail.com>
2022-09-24 16:23:56 +0300

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(PATH, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='browser_extension',
-    version='0.1.2',
+    version='0.1.3',
     description='Download browser extensions',
     long_description=long_description,
     long_description_content_type='text/markdown',
